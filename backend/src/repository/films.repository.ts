2fc69675ca5d70760291ff331f9fa0fcdd1d8bfd
Ekaterina Novikova <fearnocait@gmail.com>
2026-02-22 20:48:32 +0300

    [feat] add repository for get films

---
+++ backend/src/repository/films.repository.ts
@@ -0,0 +1,54 @@
+import { Injectable } from '@nestjs/common';
+import mongoose, { Mongoose } from 'mongoose';
+import { GetFilmDto } from 'src/films/dto/films.dto';
+
+const ScheduleSchema = new mongoose.Schema({
+  id: { type: String, required: true },
+  daytime: { type: String, required: true },
+  hall: { type: Number, required: true },
+  rows: { type: Number, required: true },
+  seats: { type: Number, required: true },
+  price: { type: Number, required: true },
+});
+
+const FilmSchema = new mongoose.Schema({
+  id: { type: String, required: true },
+  rating: { type: Number, required: true },
+  director: { type: Number, required: true },
+  tags: { type: [String], required: true },
+  image: { type: String, required: true },
+  cover: { type: String, required: true },
+  title: { type: String, required: true },
+  about: { type: String, required: true },
+  description: { type: String, required: true },
+  schedule: { type: [ScheduleSchema], required: true },
+});
+
+const Film = mongoose.model('Film', FilmSchema);
+
+export default Film;
+
+@Injectable()
+export class FilmsMongoDbRepository {
+  constructor(private connection: Mongoose) {}
+  private getFilmMapperFn(): (Film) => GetFilmDto {
+    return (root) => {
+      return {
+        id: root.id,
+        rating: root.rating,
+        director: root.director,
+        title: root.title,
+        about: root.about,
+        description: root.description,
+        image: root.image,
+        cover: root.cover,
+        tags: root.tags,
+      };
+    };
+  }
+
+  async findAll(): Promise<GetFilmDto[]> {
+    const items = await Film.find({});
+    return items.map(this.getFilmMapperFn());
+  }
+}
