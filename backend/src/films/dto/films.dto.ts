10c3d1d5f4e93bd3a880545c73065874bf46d75f
Ekaterina Novikova <fearnocait@gmail.com>
2026-02-22 20:05:58 +0300

    [feat] add dtos for get films

--- backend/src/films/dto/films.dto.ts
+++ backend/src/films/dto/films.dto.ts
@@ -1,1 +1,17 @@
 //TODO описать DTO для запросов к /films
+export class GetFilmDto {
+  id: string;
+  rating: number;
+  director: string;
+  tags: string[];
+  title: string;
+  about: string;
+  description: string;
+  image: string;
+  cover: string;
+}
+
+export class GetFilmsDto {
+  total: number;
+  items: GetFilmDto[];
+}
