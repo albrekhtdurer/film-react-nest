fdccbf788657c3a320a74c2d3b0f18e0f7a2d672
Ekaterina Novikova <fearnocait@gmail.com>
2026-03-25 12:56:41 +0300

    [fix] add tests to json logger

--- backend/src/loggers/json.logger.ts
+++ backend/src/loggers/json.logger.ts
@@ -6,15 +6,15 @@ export class JsonLogger implements LoggerService {
     return JSON.stringify({ level, message, optionalParams });
   }
 
-  log(message: any, optionalParams: any[]) {
-    console.log(this.formatMessage('log', message, optionalParams));
+  log(message: any, ...optionalParams: any[]) {
+    console.log(this.formatMessage('log', message, ...optionalParams));
   }
 
-  error(message: any, optionalParams: any[]) {
-    console.error(this.formatMessage('error', message, optionalParams));
+  error(message: any, ...optionalParams: any[]) {
+    console.error(this.formatMessage('error', message, ...optionalParams));
   }
 
-  warn(message: any, optionalParams: any[]) {
-    console.warn(this.formatMessage('warn', message, optionalParams));
+  warn(message: any, ...optionalParams: any[]) {
+    console.warn(this.formatMessage('warn', message, ...optionalParams));
   }
 }
